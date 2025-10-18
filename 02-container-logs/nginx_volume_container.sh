9191e53624193140957bb3519b30d36f1a57a4fe
Emanuel <emanuelmarcos9800@gmail.com>
2025-10-18 16:59:40 -0300

    Executando desafio 2

---
+++ 02-container-logs/nginx_volume_container.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Passo 1: Criando o volume Docker chamado 'nginx_logs'..."
+docker volume create nginx_logs
+
+echo "Passo 2: Executando o primeiro container Nginx ('nginx-com-volume')..."
+docker container run --name nginx-com-volume -d -p 8080:80 --mount type=volume,source=nginx_logs,target=/var/log/nginx nginx
+
+echo "Passo 3: Gerando logs de acesso com o comando 'curl http://localhost:8080'..."
+curl http://localhost:8080
+
+echo "Passo 4: Parando e removendo o container 'nginx-com-volume'..."
+docker stop nginx-com-volume
+docker rm nginx-com-volume
+
+echo "O container foi removido, mas o volume 'nginx_logs' e seus dados ainda existem!"
+
+echo "Passo 5: Criando um NOVO container para validar a persistencia dos logs..."
+docker container run --name nginx-validador -d -p 8080:80 --mount type=volume,source=nginx_logs,target=/var/log/nginx nginx
+
+echo "Passo 6: Verificando o conteudo do 'access.log' dentro do NOVO container..."
+docker exec nginx-validador cat /var/log/nginx/access.log
+
+echo "Limpeza: Parando e removendo o container de validacao..."
+docker nginx-validador
+docker nginx-validador
+
+echo "Concluído"
